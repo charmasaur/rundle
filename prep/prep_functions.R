3c727a21a03e7aa9d482ffb90d39faa5777b7a8e
mrsparklesaur <rslatyer@gmail.com>
2022-04-10 20:56:57 +1000

    create filled in elevation profiles

--- prep/prep_functions.R
+++ prep/prep_functions.R
@@ -33,63 +33,70 @@ makePlots <- function(file, path){
   lats <- as.numeric(coords["lat",])
   lons <- as.numeric(coords["lon",])
   
+  # get start lat/lon
+  start <- c(lons[1], lats[1])
+  
   # elevation
   elev <- as.numeric(xpathSApply(gfile, path = "//trkpt/ele", xmlValue))
   
-  # put into data frame
-  gdf <- data.frame(row = c(1:length(elev)),
-                    elev = elev,
-                    lat = lats,
-                    lon = lons) %>%
-    mutate(elev.l = lowess(elev, f = 0.001)$y)
-  
-  # convert to spatial for distance calcs
-  start <- c(lons[1], lats[1])
-  
-  # make some plots
-  ## elevation
-  svg(file.path(path, paste0(file, "_elev.svg")))
-  
-  plot(gdf$elev.l, 
-       type = "l", 
-       bty = "n", 
-       ylab = "", 
-       xlab = "", 
-       col = "grey40",
-       axes = F,
-       lwd = 2)
-  
-  dev.off()
-  
-  ## scaled elevation
-  svg(file.path(path, paste0(file, "_elev_scaled.svg")))
-  
-  plot(gdf$elev.l, 
-       type = "l", 
-       bty = "n", 
-       ylab = "", 
-       xlab = "", 
-       col = "grey40",
-       axes = F,
-       lwd = 2,
-       ylim = c(min(gdf$elev.l), min(gdf$elev.l)+1500))
-  
-  dev.off()
-  
-  ## map
-  svg(file.path(path, paste0(file, "_course.svg")))
-  
-  plot(rev(gdf$lon), 
-       rev(gdf$lat), 
-       type = "l", 
-       col = "red", 
-       lwd = 2, 
-       bty = "n",
-       axes = F,
-       xlab = "",
-       ylab = "")
-  
-  dev.off()
+  if(length(elev) != 0){ # sometimes the gpx doesn't have elevation
+    # put into data frame
+    gdf <- data.frame(row = c(1:length(elev)),
+                      elev = elev,
+                      lat = lats,
+                      lon = lons) %>%
+      mutate(elev.l = lowess(elev, f = 0.001)$y)
+    
+    # make some plots
+    ## elevation
+    svg(file.path(path, paste0(file, "_elev.svg")))
+    
+    plot(gdf$elev.l, 
+         type = "l", 
+         bty = "n", 
+         ylab = "", 
+         xlab = "", 
+         col = "grey40",
+         axes = F,
+         lwd = 2)
+    
+    dev.off()
+    
+    ## scaled elevation
+    svg(file.path(path, paste0(file, "_elev_scaled.svg")))
+    
+    plot(gdf$elev.l, 
+         type = "l", 
+         bty = "n", 
+         ylab = "", 
+         xlab = "", 
+         col = "grey40",
+         axes = F,
+         lwd = 2,
+         ylim = c(min(gdf$elev.l), min(gdf$elev.l)+1500))
+    
+    polygon(c(1, 1:nrow(gdf), nrow(gdf)+1),
+            c(min(gdf$elev.l), gdf$elev.l, min(gdf$elev.l)),
+            col="grey", 
+            border=F)
+    
+    dev.off()
+    
+    ## map
+    svg(file.path(path, paste0(file, "_course.svg")))
+    
+    plot(rev(gdf$lon), 
+         rev(gdf$lat), 
+         type = "l", 
+         col = "red", 
+         lwd = 2, 
+         bty = "n",
+         axes = F,
+         xlab = "",
+         ylab = "")
+    
+    dev.off()
+  }
   
   return(start)
 }
