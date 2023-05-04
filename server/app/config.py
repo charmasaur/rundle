7dc34005a4754c5b53a4a58b28b7d388840425b3
Harry Slatyer <harry.slatyer@gmail.com>
2023-05-04 22:12:06 +1000

    Start pre-pinging to try to avoid DB errors

--- server/app/config.py
+++ server/app/config.py
@@ -5,3 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # To try to avoid the Neon errors I sometimes get.
+    SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
